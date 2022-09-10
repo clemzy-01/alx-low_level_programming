6b3f916e9af1ba8045655b00542bc1835bb72a97
clemzy-01 <clemchidieberemordi@gmail.com>
2022-09-10 16:26:34 -0700

    Program Success in Combination

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -5,7 +5,7 @@
  * Return: Always Return 0
  *
  */
-int main()
+int main(void)
 {
 	int i;
 	int j;
@@ -17,9 +17,9 @@ int main()
 		{
 			for (k = 0; k <= 9; k++)
 			{
-				if (k > j && j>> i)
+				if (k > j && j > i)
 				{
-					putchar (i +'0');
+					putchar (i + '0');
 					putchar (j + '0');
 					putchar (k + '0');
 					if (i != 7 || j != 8 || k != 9)
@@ -31,6 +31,6 @@ int main()
 			}
 		}
 	}
-	putchar('\n');					
+	putchar('\n');
 	return (0);
 }
