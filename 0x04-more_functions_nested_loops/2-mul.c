091b269861fe3d9ae624a9dfbcb0a5ce85a2737d
clemzy-01 <clemchidieberemordi@gmail.com>
2022-09-15 19:13:41 -0700

    progressive

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -4,11 +4,12 @@
  * mul - multiplier
  *
  * @a: integer to multiply
+ *
  * @b: integer to multiply
  *
  * Return: result of two
  */
 int mul(int a, int b)
 {
-	return(a * b);
+	return (a * b);
 }
