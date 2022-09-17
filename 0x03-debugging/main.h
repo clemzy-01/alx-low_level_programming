8ef605e7571346c73848209181c3099bc3cbf635
clemzy-01 <clemchidieberemordi@gmail.com>
2022-09-16 20:38:18 -0700

    task 2 complete

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -5,16 +5,18 @@
 
 void positive_or_negative(int i);
 /**
- * positive_or_negative - function to return that integer is zero              * @i: integer declared
- * Return: largest number                                                     
- */  
+ * positive_or_negative - function to return that integer is zero
+ * @i: integer declared
+ * Return: largest number
+ */
 
 int largest_number(int a, int b, int c);
-/**                                                                 
- * largest_number - returns the largest of 3 numbers                           * @a: first integer                                                          
- * @b: second integer                                                         
- * @c: third integer                                                          
- * Return: largest number                                                     
- */  
+/**
+ * largest_number - returns the largest of 3 numbers
+ * @a: first integer
+ * @b: second integer
+ * @c: third integer
+ * Return: largest number
+ */ 
 
 #endif /* MAIN_H */
