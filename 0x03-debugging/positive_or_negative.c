ad5198c992c03645d050f3738d95e856db761b6c
clemzy-01 <clemchidieberemordi@gmail.com>
2022-09-16 18:43:44 -0700

    task 0 done

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * positve_negative - main function
+ *@i: integer to check
+ *
+ * Return: always 0
+ */
+ 
+void positive_or_negative(int i)
+{
+	/*srand(time(0));
+	i = rand() - RAND_MAX / 2;*/
+
+	printf("%d is zero\n", i);
+}
