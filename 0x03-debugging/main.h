5e11044f73dc9a0276662457fe33c8df214baeea
clemzy-01 <clemchidieberemordi@gmail.com>
2022-09-16 21:41:24 -0700

    task 3 done

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -19,4 +19,23 @@ int largest_number(int a, int b, int c);
  * Return: largest number
  */ 
 
+int convert_day(int month, int day);
+/*
+ * convert_day - converts day of month to day of year, without accounting
+ * for leap year
+ * @month: month in number format
+ * @day: day of month
+ * Return: day of year
+ */
+
+void print_remaining_days(int month, int day, int year);
+/**
+ * print_remaining_days - takes a date and prints how many days are
+ * left in the year, taking leap years into account
+ * @month: month in number format
+ * @day: day of month
+ * @year: year
+ * Return: void
+ */
+
 #endif /* MAIN_H */
