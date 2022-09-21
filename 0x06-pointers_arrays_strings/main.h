f192d4ee93415df4b77b2f587debdf8278a2065a
clemzy-01 <clemchidieberemordi@gmail.com>
2022-09-21 15:24:31 -0700

    final works

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -337,4 +337,27 @@ void swap_int(int *a, int *b);
 
 int _strlen(char *s);
 
+char *add_strings(char *n1, char *n2, char *r, int r_index);                         
+                                                                                   
+char *infinite_add(char *n1, char *n2, char *r, int size_r);                         
+/**                                                                                  
+ * add_strings - Adds the numbers stored in two strings.                             
+ * @n1: The string containing the first number to be added.                          
+ * @n2: The string containing the second number to be added.                         
+ * @r: The buffer to store the result.                                               
+ * @r_index: The current index of the buffer.                                        
+ *
+ * Return: If r can store the sum - a pointer to the result.                         
+ * If r cannot store the sum - 0.                                                    
+ */    
+
+void print_buffer(char *b, int size);
+ /**                                                                        
+  * print_buffer - Prints a buffer
+  * @b: The buffer to be printed.                                           
+  * @size: The number of bytes to be printed from the buffer.               
+  *                                                                         
+  */                                                                        
+ 
+
 #endif
