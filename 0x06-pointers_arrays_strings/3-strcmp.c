acbd804460e582ead90dca606d76898738622c20
clemzy-01 <clemchidieberemordi@gmail.com>
2022-09-21 13:21:27 -0700

    String Compare

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -3,14 +3,11 @@
 /**
  *
  * _strcmp - string to compare
- *
  * @s1: first string
  * @s2: second string
  *
  * Return: value of string
- *
  */
-
 int _strcmp(char *s1, char *s2)
 {
 	int i = 0;
@@ -19,5 +16,5 @@ int _strcmp(char *s1, char *s2)
 	{
 		i++;
 	}
-	return (s1[i] - s2[i]);
+	return ((s1[i] - s2[i]));
 }		
