bdc4f49ce9799ed9b5513943a0ced47fe149fcac
clemzy-01 <clemchidieberemordi@gmail.com>
2022-10-04 17:24:22 -0700

    commit done

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,24 +1,31 @@
-#ifndef _MAIN_H
-#define _MAIN_H
+#include "main.h"
+#include <stdlib.h>
 
 /**
- * File: main.h
- * Desc: Header file containing prototypes for all functions
- * used in the 0x0A-malloc_free directory.
+ * create_array - Creates an array of chars and
+ * initializes it with a specific char.
+ * @size: The size of the array to be initialized.
+ * @c: The specific char to intialize the array with.
+ * Return: If size == 0 or the function fails - NULL.
+ * Otherwise - a pointer to the array.
  */
+char *create_array(unsigned int size, char c)
+{
+	char *array;
+	unsigned int index;
 
-char *create_array(unsigned int size, char c);
-
-char *_strdup(char *str);
-
-char *str_concat(char *s1, char *s2);
-
-int **alloc_grid(int width, int height);
-
-void free_grid(int **grid, int height);
-
-char *argstostr(int ac, char **av);
-
-char **strtow(char *str);
-
-#endif
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	array = malloc(sizeof(char) * size);
+	if (array == NULL)
+	{
+		return (NULL);
+	}
+	for (index = 0; index < size; index++)
+	{
+		array[index] = c;
+	}
+	return (array);
+}
